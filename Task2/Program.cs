6ff0808c8c354e5624821eaa82dc42775f7e7b48
yanepumba <adress>
2023-11-16 21:00:19 +0300

    added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на 
+// вход число и возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int Sum (int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    
+    return Sum(n/10)+n%10;
+}
+
+int n=ReadInt("Введите число: ");
+Console.WriteLine(Sum(n));
