4500f4414489fb096678d95cc824c1f86af827d7
yanepumba <adress>
2023-11-16 21:21:29 +0300

    added Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    int result=Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool Power2(int a)
+{
+    if(a==1)
+    {
+        return true;
+    }
+    return a%2==0 && Power2(a/2);
+}
+
+int a=ReadInt("Введите число: ");
+Console.WriteLine(Power2(a));
